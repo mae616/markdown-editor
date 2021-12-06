f611b4fe59819a4cb2679653b59982e582e3aa3a
mae616 <oya2.mi@hotmail.co.jp>
2021-12-06 14:30:22 +0900

    webpack-dev-serverの導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { render } from 'react-dom';
 
-const Main = (<h1>Markdown Editor</h1>);
+const Main = (<h1>Markdown Editor + Dev server</h1>);
 
 render(Main, document.getElementById('app'));
