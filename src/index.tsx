15db54b50eb5f88629324aa85aaf665eaa55c694
mae616 <oya2.mi@hotmail.co.jp>
2021-12-08 11:42:03 +0900

    エディタ画面の見た目を作る

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import { render } from 'react-dom';
 import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import { Editor } from './pages/editor';
 
-const Header = styled.h1`
-    color: red;
+const GlobalStyle = createGlobalStyle`
+    body * {
+        box-sizing: border-box;
+    }
 `;
 
-const Main = (<Header>Markdown Editor</Header>);
+const Main = (
+    <>
+        <GlobalStyle />
+        <Editor />
+    </>
+);
 
 render(Main, document.getElementById('app'));
