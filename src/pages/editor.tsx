cf0f84a727ff868e46e80cd76b3bf5495267db4b
mae616 <oya2.mi@hotmail.co.jp>
2021-12-09 20:39:43 +0900

    リンクを使ってページ遷移を試してみる

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -5,6 +5,7 @@ import ReactMarkdown from 'react-markdown';
 import { putMemo } from '../indexeddb/memos';
 import { Button } from '../components/button';
 import { SaveModal } from '../components/save_modal';
+import { Link } from 'react-router-dom';
 
 const Header = styled.header`
     align-center: center;
@@ -77,6 +78,9 @@ export const Editor: React.FC = () =>{
                     <Button onClick={() => setShowModal(true)}>
                         保存する
                     </Button>
+                    <Link to='/history'>
+                        履歴を見る
+                    </Link>
                 </HeaderControl>
             </Header>
 
